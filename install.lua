2b484f2c4d4438a3901d0ede625a7ede540684bc
meow-meow-launcher <ktot96908@gmail.com>
2025-05-05 17:26:03 +0300

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,3 +1,4 @@
-shell.run("wget run https://basalt.madefor.cc/install.lua packed")
+shell.run("wget run https://raw.githubusercontent.com/Pyroxenium/Basalt/refs/heads/master/docs/install.lua release [remote_filename] [local_filename]")
+shell.run("wget run https://raw.githubusercontent.com/Pyroxenium/Basalt/refs/heads/master/docs/install.lua release latest.lua")
 shell.run("wget https://meow-meow-launcher.github.io/JFX-launcher-cdn/player.lua")
 print("Installation complete. Run: player")

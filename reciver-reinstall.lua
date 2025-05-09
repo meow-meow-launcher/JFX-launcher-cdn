794f1c26e3892f96b32edda20aa49b574f918889
meow-meow-launcher <ktot96908@gmail.com>
2025-05-09 22:02:41 +0300

    Update reciver-reinstall.lua

--- reciver-reinstall.lua
+++ reciver-reinstall.lua
@@ -1,3 +1,3 @@
 shell.run("delete player-reciver.lua")
 shell.run("wget https://meow-meow-launcher.github.io/JFX-launcher-cdn/player-reciver.lua")
-print("Installation complete. Run: player-reciver")
+os.reboot
